fb7f1406318e4825dba77c37016f70d27089d98d
budiboi22 <arif@budiboi.com>
2014-06-02 09:42:05 +0700

    testing

---
+++ app/views/layout/client.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>@yield('title')</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" media="screen">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" media="screen">
+	<link href="{{ asset('foto/favicon.ico') }}" rel="shortcut icon" />	
+  </head>
+  <body>
+
+    <!-- .container -->
+    <div class="container-fluid">
+
+      <!-- .row -->
+      <div class="row">
+
+        <!-- #konten -->
+        <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9" id="konten">
+          @yield('konten')
+        </div>
+        <!-- /#konten -->
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- .container -->
+
+    <!-- modal -->
+    @include('includes.modal')
+
+    <!-- url -->
+    @include('includes.url')
+
+<!-- start footer-->
+	@include('includes.footer')
+<!-- end footer-->
+
+	
+    <!-- JavaScript -->
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
+  </body>
+</html>
